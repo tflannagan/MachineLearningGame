15815d944c3d4435151151a6a9428ae5b21ded9c
Ty Flannagan <tylerflannagan@yahoo.com>
2022-07-14 08:33:03 -0700

    Updated bg color and buttons

--- main.js
+++ main.js
@@ -29,13 +29,16 @@ const traffic = [
   new Car(road.getLaneCenter(0), -500,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(1), -500,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(1), -700,30,50, "DUMMY", 2,getRandomColor()),
-  new Car(road.getLaneCenter(2), -700,30,50, "DUMMY", 2,
-  getRandomColor()),
+  new Car(road.getLaneCenter(2), -700,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(0), -1100,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(2), -1100,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(1), -1400,30,50, "DUMMY", 2,getRandomColor()),
   new Car(road.getLaneCenter(1), -1600,30,50, "DUMMY", 2,getRandomColor()),
-  new Car(road.getLaneCenter(0), -1700,30,50, "DUMMY", 2,getRandomColor())
+  new Car(road.getLaneCenter(0), -1700,30,50, "DUMMY", 2,getRandomColor()),
+  new Car(road.getLaneCenter(2), -1850,30,50, "DUMMY", 2,getRandomColor()),
+  new Car(road.getLaneCenter(1), -1900,30,50, "DUMMY", 2,getRandomColor()),
+  new Car(road.getLaneCenter(1), -2000,30,50, "DUMMY", 2,getRandomColor()),
+  new Car(road.getLaneCenter(0), -2100,30,50, "DUMMY", 2,getRandomColor())
 ];
 
 
